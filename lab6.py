5a10e944f6cde1972265f9822fcf099552ad96ef
helemzh <hajzhang@gmail.com>
2023-10-25 11:01:38 -0400

    added encoder function

--- lab6.py
+++ lab6.py
@@ -1,3 +1,24 @@
 # Helen Zhang
+# encode 6-digit password shift up 3 num
+def encoder(string):
+    pw = ""
+    for c in string:
+        if c == "9":
+            pw += "2"
+        elif c == "8":
+            pw += "1"
+        elif c == "7":
+            pw += "0"
+        else:
+            pw += str(int(c) + 3)
+    return pw
 
-# encode password
+
+def main():
+    print(encoder("12345555"))
+    print(encoder("00009962"))
+
+
+if __name__ == '__main__':
+    main()
+
